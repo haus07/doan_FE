b302a860a11443a7599c0b1b5879434efa46fc11
haus0409 <haconghau07@gmail.com>
2025-05-10 22:48:07 +0700

    xong 1 phan giao dien va 1 vai chuc nang nho

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,29 @@
+import React from "react";
+import { assets } from "../assets/assets";
+import { useNavigate } from "react-router-dom";
+
+const Navbar = () => {
+    const navigate = useNavigate();
+    return (
+        <>
+            <div className="w-full flex justify-between items-center font-semibold">
+                <div className="flex items-center gap-2">
+                    <img onClick={()=>navigate(-1)} className="w-8 bg-black p-2 rounded-full cursor-pointer" src={ assets.arrow_left } alt="" />
+                    <img onClick={()=>navigate(1)} className="w-8 bg-black p-2 rounded-full cursor-pointer" src={ assets.arrow_right } alt="" />
+                </div>
+                <div className="flex items-center gap-4">
+                    <p className="bg-white text-black text-[15px] px-4 py-1 rounded-full hidden md:block">Explore Prenium</p>
+                    <p className="bg-black py-1 px-3 rounded-full text-[15px]">Install App</p>    
+                    <p className="bg-purple-500 text-black w-7 h-7 rounded-full flex items-center justify-center"></p>
+                </div>
+            </div>
+            <div className="flex items-center gap-2 mt-4">
+                <p className="bg-white text-black px-4 py-1 rounded-full">All</p>
+                <p className="bg-black px-4 py-1 rounded-full">Music</p>
+                <p className="bg-black px-4 py-1 rounded-full">Podcasts</p> 
+            </div>
+        </>
+    )
+}
+
+export default Navbar
